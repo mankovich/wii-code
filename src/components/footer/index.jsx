2314647ac8822ece02a343744368f310fcd315c5
Anamaris Ortiz <anamarisortiz@hotmail.com>
2024-09-05 14:04:13 -0400

    login signup and footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,20 +1,21 @@
-import Button from 'react-bootstrap/Button'
-import { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useLocation } from 'react-router-dom';
-import './style.css'
+import './style.css';
 
 function Footer() {
+    const location = useLocation();
 
-    const [render, setRender] = useState('');
-    /*TODO: ?? */
+    
+    const isEditor = location.pathname === '/editor' || location.pathname.startsWith('/editor/');
 
-    //button-loading state to give loading feedback to user during async render operation...
     const [isLoading, setLoading] = useState(false);
+
     useEffect(() => {
-        //placeholder until the real routing function is available (I think)....
+        
         function simulateRequest() {
             return new Promise((resolve) => setTimeout(resolve, 5000));
         }
@@ -28,24 +29,8 @@ function Footer() {
 
     function handleClick() {
         setLoading(true);
-        /*TODO:*/
+        //add logic for rendering code think its just going to be window.location'pathname'
     }
-
-    // const [btn, setBtnShow] = useState(false);
-
-    // function getPage() {
-    //     let location = useLocation()
-    //     console.log(location)
-        
-    // }
-
-    // function setBtn() {
-    //     console.log(window.location.pathname)
-    //     btn = useState(window.location.pathname === '/editor/*' ? 'visible' : 'hidden');
-    //     return btn
-    //     /* FIXME: HELP! lol */
-    // }
-
 
     return (
         <>
@@ -61,23 +46,24 @@ function Footer() {
                             </div>
                         </Col>
                         <Col xs={3}>
-                            <Button
-                                variant='primary'
-                                size='sm'
-                                id='render-btn'
-                                // visibility={() => window.location.pathname === '/editor/*' ? 'visible' : 'hidden'}
-                                /*FIXME: HELP ME lol */
-                                disabled={isLoading}
-                                onClick={!isLoading ? handleClick : null}
-                            >
-                                {isLoading ? 'Loading...' : 'Render code'}
-                            </Button>
+                          
+                            {isEditor && (
+                                <Button
+                                    variant='primary'
+                                    size='sm'
+                                    id='render-btn'
+                                    disabled={isLoading}
+                                    onClick={!isLoading ? handleClick : null}
+                                >
+                                    {isLoading ? 'Loading...' : 'Render code'}
+                                </Button>
+                            )}
                         </Col>
                     </Row>
                 </Container>
             </div>
         </>
-    )
+    );
 }
 
 export default Footer;
