606b0bc9145150c1bd45c41856190356d9921a30
Daniel DeMoney <weston@MacBook-Air.home>
2024-09-05 14:52:01 -0700

    Added upload file functionality

--- src/components/uploadFile/index.jsx
+++ src/components/uploadFile/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal'
@@ -6,6 +6,7 @@ import Stack from 'react-bootstrap/Stack'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
 import { faArrowUpFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { fileToText } from '../../utils/fileToArray';
 import './style.css'
 
 function UploadFile() {
@@ -14,8 +15,35 @@ function UploadFile() {
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const fileInputRef = useRef(null);
 
-    const handleUploadFile = () => {
+    const handleUploadFile = async (e) => {
+        e.preventDefault();
+        console.log(e);
+        const content = await fileToText(fileInputRef.current.files[0]);
+        const file = {fileName: fileInputRef.current.files[0].name, content: content, project: 6}
+        console.log("TOKEN: " + localStorage.getItem("token"));
+        try {
+            const rereadFile = await fetch(import.meta.env.VITE_SERVER ?? "http://localhost:3001" + "/api/file", {
+                method: "POST",
+                body: JSON.stringify(file),
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                    'Content-Type': 'application/json',
+                }
+            })
+            const rereadJson = await rereadFile.json();
+            if (rereadJson.error){
+                alert(rereadJson.error);
+                return
+            }
+            console.log("Added file got back:");
+            console.log(rereadJson);
+            setDirectory([...directory, rereadJson]);
+        } catch (err) {
+            alert(err);
+            return
+        }
         setShow(false)
     }
 
@@ -32,29 +60,30 @@ function UploadFile() {
                 aria-labelledby="upload-file-modal"
                 id="upload-file-modal"
             >
-                <Modal.Header closeButton className="modal-header">
-                    <Modal.Title id="upload-file-title">
-                        Upload file
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body id="modal-body">
-                    <Form id="upload-file-form">
-                        <Form.Group controlId="formFile" className="mb-3">
-                            <Form.Label>Select file to upload</Form.Label>
-                            <Form.Control type="file" />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer id="modal-footer">
-                    <Stack direction="horizontal" gap={5}>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Cancel
-                        </Button>
-                        <Button variant="primary" onClick={handleUploadFile} id="upload-file-btn">
+                <Form id="upload-file-form" onSubmit={(e) => {handleUploadFile(e)}}>
+                    <Modal.Header closeButton className="modal-header">
+                        <Modal.Title id="upload-file-title">
                             Upload file
-                        </Button>
-                    </Stack>
-                </Modal.Footer>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body id="modal-body">
+                        
+                            <Form.Group controlId="formFile" className="mb-3">
+                                <Form.Label>Select file to upload</Form.Label>
+                                <Form.Control type="file" ref={fileInputRef} />
+                            </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer id="modal-footer">
+                        <Stack direction="horizontal" gap={5}>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="primary" type='submit' id="upload-file-btn">
+                                Upload file
+                            </Button>
+                        </Stack>
+                    </Modal.Footer>
+                </Form>
             </Modal>
         </>
     )
