fc03ea6bfb573b06cf8147f7eec329be21bee4dc
Joey M <172998670+mankovich@users.noreply.github.com>
2024-09-06 16:28:31 -0400

    create project functionality

--- src/components/newProject/index.jsx
+++ src/components/newProject/index.jsx
@@ -5,16 +5,51 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Modal from 'react-bootstrap/Modal'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom';
 import './style.css'
 
 function NewProjectModal() {
   const [show, setShow] = useState(false);
+  const [projectName, setProjectName] = useState('');
+  const navigate = useNavigate();
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const handleNewProj = () => {
-    setShow(false)
+  const handleNewProj = async () => {
+    
+
+    if (projectName === '') {
+      alert("Please enter a project name!");
+      return;
+    }
+    const project = { projectName: projectName, ownerId: localStorage.getItem('userId') }
+    console.log("TOKEN: " + localStorage.getItem("token"));
+    try {
+      const rereadProject = await fetch(import.meta.env.VITE_SERVER + "/api/project", {
+        method: "POST",
+        body: JSON.stringify({ project: project, files: [{ fileName: 'index.html', content: '' }]}),
+        headers: {
+          Authorization: localStorage.getItem("token"),
+          'Content-Type': 'application/json',
+        }
+      })
+      console.log(`\n\n\nlets see\n\n\n`)
+      console.log(rereadProject)
+      const rereadJson = await rereadProject.json();
+      if (rereadJson.error) {
+        alert(rereadJson.error);
+        return
+      }
+      console.log("Added project got back:");
+      console.log(rereadJson);
+      
+      
+      navigate(`/editor/${rereadJson.project.ID}`)
+    } catch (err) {
+      alert(err);
+      return
+    }
   }
 
   return (
@@ -43,13 +78,17 @@ function NewProjectModal() {
                 label="New project name"
                 className="mb-3"
               >
-                <Form.Control type="text" placeholder="New project name" autoFocus />
+                <Form.Control 
+                  type="text" 
+                  placeholder="New project name"
+                  value={projectName} 
+                  onChange={(e)=>{setProjectName(e.target.value)}}/>
               </FloatingLabel>
             </Form.Group>
             {/* <Form.Group controlId="formFileMutltiple" className="mb-3">
-              <Form.Label>Select file(s) to upload to new project</Form.Label>
-              <Form.Control type="file" multiple />
-            </Form.Group> */}
+                <Form.Label>Select file(s) to upload to new project</Form.Label>
+                <Form.Control type="file" multiple />
+              </Form.Group> */}
           </Form>
         </Modal.Body>
         <Modal.Footer id="modal-footer">
