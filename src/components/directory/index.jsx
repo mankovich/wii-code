9374744313b150879392e2bdeda75aec3c7dfcc9
Joey M <172998670+mankovich@users.noreply.github.com>
2024-09-06 20:59:23 -0400

    files in directory highlight on hover and displays name of current file on click

--- src/components/directory/index.jsx
+++ src/components/directory/index.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 import './style.css'
 
 function Directory (props) {
+  const [displayName, setDisplayName] = useState('')
+    
   function setFile(file) {
     console.log(file);
     props.setCurrentFile(file);
+    setDisplayName(file.fileName);
   }
 
   return (
     <>
                 <div id="dir-div">
                 <h5 id="dir-heading">File Directory</h5>
+                <p id="current-name"><FontAwesomeIcon icon={faPenToSquare} />: {displayName}</p>
                 <ul>
                 {props.files.map((file) => (
-                    <li key={file.id} style={{color:"white", cursor: "pointer"}} onClick={() => setFile(file)}>{`${file.fileName}`}</li>
+                    <li key={file.id} id="file-li" onClick={() => setFile(file)}>{`${file.fileName}`}</li>
                     // <li style={{color:"white"}}>{`${file}`}</li>
                 ))}
                 </ul>
